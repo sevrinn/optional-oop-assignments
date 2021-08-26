aea9f0333e5963462afa662bbde5ea9488ece6eb
Sevrinn <sevrinnsevrinn@gmail.com>
2021-08-25 21:50:36 -0700

    extended ninja class with a sensei class

--- Ninja.js
+++ Ninja.js
@@ -1,9 +1,17 @@
 class Ninja {
-  constructor(name, health) {
+  constructor(name, health, speed, strength) {
     this.name = name;
     this.health = health;
-    this.speed = 3;
-    this.strength = 3;
+    if (speed) {
+      this.speed = speed;
+    } else {
+      this.speed = 3;
+    }
+    if (strength) {
+      this.strength = strength;
+    } else {
+      this.strength = 3;
+    }
   }
 
   //ninja methods
@@ -22,8 +30,25 @@ class Ninja {
   }
 }
 
-const ninja1 = new Ninja("Hyabusa", 100);
+class Sensei extends Ninja {
+  constructor(name) {
+    super(name, 200, 10, 10);
+  }
+
+  speakWisdom() {
+    console.log("Be kind to yourself, you're doing better than you think!");
+    this.drinkSake();
+  }
+}
+
+const ninja1 = new Ninja("Hyabusa", 100, 4);
 ninja1.sayName();
 ninja1.showStats();
 ninja1.drinkSake();
 ninja1.showStats();
+
+const ninja2 = new Sensei("Goku");
+ninja2.sayName();
+ninja2.showStats();
+ninja2.speakWisdom();
+ninja2.showStats();
